ce11cc91cd84fa3fc1cb10cf655681ded2a84856
togekk <togekk@gmail.com>
2018-11-07 11:54:33 +0800

    Create benchmark for wasm-worker

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,7 +1,24 @@
 import 'allocator/arena';
 export { memory };
 
-export function get_array(len: i32): Float64Array {
-  const a = new Float64Array(len);
-  return a;
+export function fib_loop(): void {
+  for (let i = 0; i < 10000; i++) fib(0, 1, i);
+}
+
+function fib(curr: f64, next: f64, n: f64): f64 {
+  if (n == 0) {
+    return curr;
+  }
+  else {
+    return fib(next, curr + next, n - 1);
+  }
+}
+
+export function fib_ext(curr: f64, next: f64, n: f64): f64 {
+  if (n == 0) {
+    return curr;
+  }
+  else {
+    return fib(next, curr + next, n - 1);
+  }
 }
